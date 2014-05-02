7a7c6cf0f12f852db69a18fc80bfbef0767df417
r-a-y <r-a-y@gmx.com>
2014-05-01 21:50:21 -0700

    Add inbound provider classes.
    - Includes a new abstract class method - BP_Reply_By_Email_Inbound_Provider -
      which handles registering an inbound provider to RBE when extended
    - Includes a default inbound provider - Mandrill
    
    See #46.

--- includes/bp-rbe-classes.php
+++ includes/bp-rbe-classes.php
@@ -1164,6 +1164,177 @@ class BP_Reply_By_Email_Parser {
 }
 
 /**
+ * Abstract base class for adding an inbound email provider.
+ *
+ * @since 1.0-RC3
+ */
+abstract class BP_Reply_By_Email_Inbound_Provider {
+	/**
+	 * @var array An array containing names and types of abstract properties that must
+	 *      be implemented in child classes.
+	 */
+	private $_abstract_properties = array(
+		'name' => array(
+			'type'   => 'string',
+			'static' => true,
+		),
+	);
+
+	/**
+	 * Constructor.
+	 */
+	final public function __construct() {
+		$this->_abstract_properties_validate();
+	}
+
+	/**
+	 * Make sure our class properties exist in extended classes.
+	 *
+	 * PHP doesn't accept abstract class properties, so this class method adds
+	 * this capability.
+	 */
+	final protected function _abstract_properties_validate() {
+		//check if the child class has defined the abstract properties or not
+		$child = get_class( $this );
+
+		foreach ( $this->_abstract_properties as $name => $settings ) {
+			if ( isset( $settings['type'] ) && 'string' == $settings['type'] ) {
+				if ( isset( $settings['static'] ) && true === $settings['static'] ) {
+					$prop = new ReflectionProperty( $child, $name );
+
+					if ( ! $prop->isStatic() ) {
+						// property does not exist
+						$error = $child . ' class must define $' . $name . ' property as static ' . $settings['type'];
+						unset( $prop, $child );
+						throw new \LogicException( $error );
+					}
+				} else {
+
+					if ( property_exists( $this, $name ) && strtolower( gettype( $this->$name ) ) == $settings['type'] ) {
+						continue;
+					}
+
+					// property does not exist
+					$error = $child . ' class must define $' . $name . ' property as ' . $settings['type'];
+
+					throw new \LogicException( $error );
+				}
+			}
+		}
+
+		unset( $error, $child );
+	}
+
+	/**
+	 * Validates post callback and posts the data on success.
+	 *
+	 * This method must exist in extended classes.
+	 */
+	abstract public static function webhook_parser();
+}
+
+/**
+ * Add Mandrill as an inbound provider for RBE.
+ *
+ * @since 1.0-RC3
+ *
+ * @see http://help.mandrill.com/entries/22092308-What-is-the-format-of-inbound-email-webhooks-
+ */
+class BP_Reply_By_Email_Inbound_Provider_Mandrill extends BP_Reply_By_Email_Inbound_Provider {
+	/**
+	 * @var string Display name for our inbound provider
+	 */
+	public static $name = 'Mandrill';
+
+	/**
+	 * Webhook parser class method for Mandrill.
+	 */
+	public static function webhook_parser() {
+		if ( empty( $_SERVER['HTTP_X_MANDRILL_SIGNATURE'] ) ) {
+			return false;
+		}
+	
+		if ( ! empty( $_SERVER['HTTP_USER_AGENT'] ) && strpos( $_SERVER['HTTP_USER_AGENT'], 'Mandrill-Webhook/' ) === false ) {
+			return false;
+		}
+	
+		bp_rbe_log( '- Mandrill webhook received -' );
+	
+		// mandrill signature verification
+		if ( defined( 'BP_RBE_MANDRILL_WEBHOOK_URL' ) && defined( 'BP_RBE_MANDRILL_WEBHOOK_KEY' ) ) {
+			$signed_data  = constant( 'BP_RBE_MANDRILL_WEBHOOK_URL' );
+			$signed_data .= 'mandrill_events';
+			$signed_data .= stripslashes( $_POST['mandrill_events'] );
+			$webhook_key  = constant( 'BP_RBE_MANDRILL_WEBHOOK_KEY' );
+	
+			$signature = base64_encode( hash_hmac( 'sha1', $signed_data, $webhook_key, true ) );
+			
+			// check if generated signature matches Mandrill's
+			if ( $signature !== $_SERVER['HTTP_X_MANDRILL_SIGNATURE'] ) {
+				bp_rbe_log( 'Mandrill signature verification failed.' );
+				die();
+			}
+	
+		}
+	
+		// get parsed content
+		$response = json_decode( stripslashes( $_POST['mandrill_events'] ) );
+	
+		// log JSON errors if present
+		if ( json_last_error() != JSON_ERROR_NONE ) {
+			switch ( json_last_error() ) {
+				case JSON_ERROR_DEPTH:
+					bp_rbe_log( 'json error: - Maximum stack depth exceeded' );
+					break;
+				case JSON_ERROR_STATE_MISMATCH:
+					bp_rbe_log( 'json error: - Underflow or the modes mismatch' );
+					break;
+				case JSON_ERROR_CTRL_CHAR:
+					bp_rbe_log( 'json error: - Unexpected control character found' );
+					break;
+				case JSON_ERROR_SYNTAX:
+					bp_rbe_log( 'json error: - Syntax error, malformed JSON' );
+					break;
+				case JSON_ERROR_UTF8:
+					bp_rbe_log( 'json error: - Malformed UTF-8 characters, possibly incorrectly encoded' );
+					break;
+				default:
+					bp_rbe_log( 'json error: - Unknown error' );
+					break;
+			}
+	
+			die();
+	
+		// ready to start the parsing!
+		} else {
+	
+			$i = 1;
+			foreach ( $response as $item ) {
+				$data = array(
+					'headers'    => $item->msg->headers,
+					'to_email'   => $item->msg->email,
+					'from_email' => $item->msg->from_email,
+					'content'    => $item->msg->text,
+					'subject'    => $item->msg->subject
+				);
+	
+				$parser = BP_Reply_By_Email_Parser::init( $data, $i );
+	
+				if ( is_wp_error( $parser ) ) {
+					do_action( 'bp_rbe_no_match', $parser, $data, $i, false );
+				}
+	
+				++$i;
+			}
+	
+			bp_rbe_log( '- Webhook parsing completed -' );
+	
+			die();
+		}
+	}
+}
+
+/**
  * Abstract base class for adding support for RBE to your plugin.
  *
  * This class relies on the activity component.  If your plugin doesn't rely
