r-a-y/bp-reply-by-email

commit 051eaf1c9c5f05b1524300865941265978fa710d

Tweaks to lock checking.
See commit 5eeb88c, #16.

includes/bp-rbe-classes.php
@@ -335,12 +335,15 @@ class BP_Reply_By_Email_IMAP {
 					wp_cache_flush();
 				}
 
-				// check if we're already attempting to connect
-				// another precaution
-				if ( bp_rbe_is_connecting() ) {
+				$gmt_time = microtime( true );
+
+				if ( bp_rbe_is_connecting( $gmt_time ) ) {
 					bp_rbe_log( '--- RBE is already attempting to connect - stopping connection attempt ---' );
-					continue;
+					return continue;
 				}
+
+				// add lock marker before connecting
+				bp_update_option( 'bp_rbe_lock', sprintf( '%.22F', $gmt_time ) );
 
 				// add lock marker before connecting
 				// add 60 second timeout, which should be enough time to connect
@@ -390,23 +393,22 @@ class BP_Reply_By_Email_IMAP {
 	private function connect() {
 		bp_rbe_log( '--- Attempting to start new connection... ---' );
 
+		$gmt_time = microtime( true );
+
+		if ( bp_rbe_is_connecting( $gmt_time ) ) {
+			bp_rbe_log( '--- RBE is already attempting to connect - stopping connection attempt ---' );
+			return false;
+		}
+
+		// add lock marker before connecting
+		bp_update_option( 'bp_rbe_lock', sprintf( '%.22F', $gmt_time ) );
+
 		// if our DB marker says we're already connected, stop now!
 		// this is an extra precaution
 		if ( bp_rbe_is_connected() ) {
 			bp_rbe_log( '--- RBE is already connected! ---' );
 			return false;
 		}
-
-		// check if we're already attemtping to connect
-		// another precaution
-		if ( bp_rbe_is_connecting() ) {
-			bp_rbe_log( '--- RBE is already attempting to connect - stopping connection attempt ---' );
-			return false;
-		}
-
-		// add lock marker before connecting
-		// add 60 second timeout, which should be enough time to connect
-		bp_update_option( 'bp_rbe_lock', time() + 60 );
 
 		// Let's open the IMAP stream!
 		$this->connection = BP_Reply_By_Email_Connect::init();

includes/bp-rbe-functions.php
@@ -145,10 +145,18 @@ function bp_rbe_is_connected() {
  *
  * @since 1.0-RC3
  *
+ * @param int|float The current UNIX timestamp to check
  * @return bool
  */
-function bp_rbe_is_connecting() {
-	if ( $lock = bp_get_option( 'bp_rbe_lock' ) && time() < $lock ) {
+function bp_rbe_is_connecting( $gmt_time = 0 ) {
+	if ( ! $gmt_time ) {
+		$gmt_time = microtime( true );
+	}
+
+	$lock = bp_get_option( 'bp_rbe_lock' );
+
+	// check if we're already attempting to connect
+	if ( $lock + WP_CRON_LOCK_TIMEOUT > $gmt_time ) {
 		return true;
 	}
 

commit 490928665c6fe186d70bba0ea919e6d01910dcfc

Fix typo introduced in commit 051eaf1.

includes/bp-rbe-classes.php
@@ -339,7 +339,7 @@ class BP_Reply_By_Email_IMAP {
 
 				if ( bp_rbe_is_connecting( $gmt_time ) ) {
 					bp_rbe_log( '--- RBE is already attempting to connect - stopping connection attempt ---' );
-					return continue;
+					continue;
 				}
 
 				// add lock marker before connecting