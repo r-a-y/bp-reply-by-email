e26b0d429eba1c974edf6f7ee4802c9f4358448d
r-a-y <r-a-y@gmx.com>
2016-02-26 12:05:39 -0800

    Inbound: Actually include our Postmark provider class!
    See #68.

---
+++ includes/classes/bp-reply-by-email-inbound-provider-postmark.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * BP Reply By Email Postmark Inbound Provider Class.
+ *
+ * @package BP_Reply_By_Email
+ * @subpackage Classes
+ */
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+/**
+ * Add Postmark as an inbound provider for RBE.
+ *
+ * @since 1.0-RC4
+ *
+ * @see http://help.mandrill.com/entries/22092308-What-is-the-format-of-inbound-email-webhooks-
+ */
+class BP_Reply_By_Email_Inbound_Provider_Postmark extends BP_Reply_By_Email_Inbound_Provider {
+	/**
+	 * @var string Display name for our inbound provider
+	 */
+	public static $name = 'Postmark';
+
+	/**
+	 * Webhook parser class method for Mandrill.
+	 */
+	public function webhook_parser() {
+		if ( empty( $_SERVER['CONTENT_TYPE'] ) || ( ! empty( $_SERVER['CONTENT_TYPE'] ) && 'application/json' !== $_SERVER['CONTENT_TYPE'] ) ) {
+			return;
+		}
+
+		if ( ! empty( $_SERVER['HTTP_USER_AGENT'] ) && 'Postmark' !== $_SERVER['HTTP_USER_AGENT'] ) {
+			return;
+		}
+
+		// Should do more checks before parsing...
+		bp_rbe_log( '- Postmark webhook received -' );
+
+
+		$response = file_get_contents( 'php://input' );
+		if ( empty( $response ) ) {
+			bp_rbe_log( '- Postmark webhook response failed -' );
+		}
+
+		$response = json_decode( $response );
+
+		// Format email headers to fit RBE spec.
+		$headers = array();
+		foreach ( $response->Headers as $header ) {
+			$headers[$header->Name] = $header->Value;
+		}
+
+		// Postmark separates parsed email headers; add them back for RBE parsing.
+		$headers['From'] = $response->From;
+		$headers['To']   = $response->To;
+
+		$data = array(
+			'headers'    => $headers,
+			'to_email'   => $response->To,
+			'from_email' => $response->From,
+			'content'    => $response->TextBody,
+			'subject'    => $response->Subject
+		);
+
+		$parser = BP_Reply_By_Email_Parser::init( $data, 1 );
+
+		if ( is_wp_error( $parser ) ) {
+			do_action( 'bp_rbe_no_match', $parser, $data, 1, false );
+		}
+
+		bp_rbe_log( '- Webhook parsing completed -' );
+		die();
+	}
+}
